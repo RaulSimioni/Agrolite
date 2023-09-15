f0bdae9d860e0adfcb245d7e941d5a1fd77876fc
Raul Simioni <raulsimioni@edu.unifil.br>
2023-09-15 15:30:05 -0300

    commit

--- Agrolite/urls.py
+++ Agrolite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("recomendacao.urls")),
-    path("recomendacao/", include("recomendacao.urls"))
+    path("recomendacao/", include("recomendacao.urls")),
 ]

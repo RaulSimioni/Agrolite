1a514759a0a2b4b057bf4e2c9a3f68f34a7dd58c
Raul Simioni <raulsimioni@edu.unifil.br>
2023-09-15 13:17:48 -0300

    Commit_FrontEnd_Fix

--- Agrolite/urls.py
+++ Agrolite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("recomendacao.urls"))
+    path("", include("recomendacao.urls")),
+    path("recomendacao/", include("recomendacao.urls"))
 ]
--- recomendacao/views.py
+++ recomendacao/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def recomendacao(request):
+    return render(request, 'recomendacao.html')
+
 # Create your views here.
